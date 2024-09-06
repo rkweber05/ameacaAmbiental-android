1ab72fa9b87b297aa889e741652a50f404a308bc
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2024-09-06 07:35:26 -0300

    modified addStudent

--- app/src/main/java/com/trabalho/ameacasambientais/AddStudent.java
+++ app/src/main/java/com/trabalho/ameacasambientais/AddStudent.java
@@ -4,17 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
+import android.widget.EditText;
 
 public class AddStudent extends AppCompatActivity {
+
+    StudentSQLiteDatabase db;
+    EditText txtEndereco, txtData, txtDesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
 
-        Window window = this.getWindow();
-        window.setStatusBarColor(this.getResources().getColor(android.R.color.white));
-        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        txtEndereco = findViewById(R.id.editTextPersonName5);
+        txtData = findViewById(R.id.editTextPersonName8);
+        txtDesc = findViewById(R.id.editTextPersonName9);
+
+        db = new StudentSQLiteDatabase
+
+                (
+                getBaseContext());
+    }
+
+    public void addStudent(View v){
+        Student s = new Student();
+        s.setEndereco(txtEndereco.getText().toString());
+        s.setData(txtData.getText().toString());
+        s.setDesc(txtDesc.getText().toString());
+
+        db.addStudent(s);
+        finish();
     }
 }
